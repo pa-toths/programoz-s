9425e7ffc19843e63580295ad92f3a7421a17128
pa-toths <112852361+pa-toths@users.noreply.github.com>
2022-10-10 09:40:18 +0200

    Add files via upload

---
+++ 11-12.óra.py
@@ -0,0 +1,21 @@
+'''
+gondolt_szám = 4
+print (type(gondolt_szám))
+tipp = input("gondoltam egy számra. Tippeld meg! ")
+tipp =int(tipp)
+if tipp == gondolt_szám:
+    print('Ügyes vagy')
+if tipp > gondolt_szám: 
+    print('a tipp nagyobb mint a gondolt szám' )
+if tipp < gondolt_szám:
+    print(' a tipp kisebb mint a gondolt szám')
+print('Pápá')
+'''
+gondolt_szám = 4 
+tipp = input("gondoltam egy számra. Tippeld meg! ")
+tipp == tipp
+if tipp == gondolt_szám:
+    print ('ügyes vagy')
+else:
+    print('nem talláltad el') 
+    print('pápá')   
