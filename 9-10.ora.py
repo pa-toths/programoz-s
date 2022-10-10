29467a32803319d3554a18d92c480997c353f8e9
pa-toths <112852361+pa-toths@users.noreply.github.com>
2022-10-10 08:33:44 +0200

    Add files via upload

--- 9-10.ora.py
+++ 9-10.ora.py
@@ -1,3 +1,13 @@
 #első feladat 9.dk
+from os import sep
+
+
 idei_ev = 2022
 print(type(idei_ev))
+print( "azidei_ev változó értéke" , idei_ev , sep='--->')
+felhasznalo_kora = input("hány éves vagy?")
+print(' a felhasználók kora változó ', type(felhasznalo_kora))
+print('Te most' , felhasznalo_kora , 'éves vagy.')
+felhasznalo_kora = int(felhasznalo_kora)
+születési_év = idei_ev - felhasznalo_kora
+print('Ekkor születtél: ' , születési_év, '.', sep='')
