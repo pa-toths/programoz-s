95abb3b05e3461b6978e3953109cf23c28e4ba90
pa-toths <112852361+pa-toths@users.noreply.github.com>
2023-01-10 13:08:30 +0100

    Add files via upload

---
+++ progalapokházifeladat.py
@@ -0,0 +1,6 @@
+lista1 = ['a', 'l', 'm', 'a', 'f', 'a']
+a_darabszam = 0
+for i in lista1:
+    if i=="a":
+        a_darabszam = a_darabszam + 1
+print(f"A lista1 {a_darabszam} darab a betüt tartakmaz: " ,)
