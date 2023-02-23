411f2450531c4b53af6ff6fd84a58c63de526f1f
pa-toths <112852361+pa-toths@users.noreply.github.com>
2023-02-23 07:45:40 +0100

    Add files via upload

---
+++ FELADAT_2.py
@@ -0,0 +1,8 @@
+# Tóth Sándor 10.d
+szam = int(input('Adj meg egy számot! '))
+if szam >=1:
+    print('A megadott szam pozitív')
+elif szam <=-1:
+    print('A megadott szám negatív')
+elif szam ==0:
+    print('A megadott szam nulla')
